47bac77126a5b92e3d5f71f040326d3ea4205afc
Jhonathan Lucas Da Silva <jhonathanlucas10x@gmail.com>
2024-07-21 18:55:52 -0300

    add context and pages SingIn,SingUp

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,12 +1,23 @@
+import { useState, useContext } from 'react';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
+import { AuthContext } from '../../contexts/auth';
 
 export default function SingUp() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {signUp, loadingAuth} = useContext(AuthContext)
+
+    async function handleSubmit(e){
+        e.preventDefault()
+
+        if(name !== '' && email !== '' && password !== ''){
+            await signUp(email, name, password);
+        }
+    }
 
     return (
         <div className="container-center">
@@ -15,7 +26,7 @@ export default function SingUp() {
                     <img src={logo} alt='Logo do sistema de chamados' />
                 </div>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1>Nova conta</h1>
 
                     <input
@@ -39,9 +50,11 @@ export default function SingUp() {
                         onChange={(e) => setPassword(e.target.value)}
                     />
 
-                    <button type="submit" >Cadastrar</button>
+                    <button type="submit" >
+                        {loadingAuth ? 'Carregando...' : 'Cadastrar'}
+                    </button>
                 </form>
-                <Link to="/"> Já possui uma conta? Faça login</Link>
+                <Link to="/"> Já possui uma conta? <strong>Faça login</strong></Link>
             </div>
         </div>
     )
